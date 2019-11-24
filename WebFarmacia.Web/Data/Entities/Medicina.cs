4cf7c129583bac5ce2b936fec191a36d30c76800
miguel <miguelantonio_rojas@hotmail.com>
2019-11-23 23:50:35 -0430

    Creando tabla Medicina

---
+++ WebFarmacia.Web/Data/Entities/Medicina.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace WebFarmacia.Web.Data.Entities
+{
+    public class Medicina
+    {
+        public int Id { get; set; }
+
+        
+        public string Name { get; set; }
+
+        
+        [DisplayFormat(DataFormatString ="{0:C2}",ApplyFormatInEditMode =false)]
+        public decimal Price { get; set; }
+
+
+
+
+
+    }
+}
