1ff5bdb50d792228ce2fd8bb99643d8a1485cf01
miguel <miguelantonio_rojas@hotmail.com>
2019-11-25 21:20:15 -0430

    Create DataContext.cs
    Crando datacontext

---
+++ WebFarmacia.Web/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebFarmacia.Web.Data
+{
+    public class DataContext
+    {
+    }
+}
