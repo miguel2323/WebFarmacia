5336b08e56e9a0e3a06f3c509cadfbcade9259d2
miguel <miguelantonio_rojas@hotmail.com>
2019-11-25 21:32:38 -0430

    Clases medicinas y datacontext lista

--- WebFarmacia.Web/Data/DataContext.cs
+++ WebFarmacia.Web/Data/DataContext.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebFarmacia.Web.Data.Entities;
 
 namespace WebFarmacia.Web.Data
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
+
+        //base de datos que se basa en el datacontext
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    
+        public DbSet<Medicina> Medicinas { get; set; }
+
+
+
+
+
     }
+
 }
--- WebFarmacia.Web/Data/Entities/Medicina.cs
+++ WebFarmacia.Web/Data/Entities/Medicina.cs
@@ -16,7 +16,25 @@ namespace WebFarmacia.Web.Data.Entities
         [DisplayFormat(DataFormatString ="{0:C2}",ApplyFormatInEditMode =false)]
         public decimal Price { get; set; }
 
+        [Display(Name ="Imagen")]
+        public string ImageUrl { get; set; }
 
+
+        [Display(Name = "Ultima Compra")]
+        public string LastPurchase{ get; set; }
+
+
+
+        [Display(Name = "Ultima venta")]//ultima venta
+        public string LastSale { get; set; }
+
+
+
+        [Display(Name = "Disponible")]
+        public string IsAvailabe{ get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+        public double stock { get; set; }
 
 
 
