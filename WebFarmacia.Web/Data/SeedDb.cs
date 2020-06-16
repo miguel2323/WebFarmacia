248de9986898516f90228e6071641e7447250113
miguel <miguelantonio_rojas@hotmail.com>
2020-06-15 23:57:33 -0430

    configurando el seedDb y el index para el manejo de usuario

--- WebFarmacia.Web/Data/SeedDb.cs
+++ WebFarmacia.Web/Data/SeedDb.cs
@@ -1,5 +1,4 @@
 ﻿
-
 /// <summary>
 /// esta clases alimenta la base de datos
 /// </summary>
@@ -11,17 +10,18 @@ namespace WebFarmacia.Web.Data
     using System.Linq;
     using System.Threading.Tasks;
     using Entities;
-
+    using Microsoft.AspNetCore.Identity;
     public class SeedDb
     {
         private readonly DataContext context;
         //inyeccion de deendencia a la base de datos
-
+        private readonly UserManager<User> userManager;
         private Random random;
 
-        public SeedDb(DataContext context)
+        public SeedDb(DataContext context, UserManager<User> userManage)
         {
             this.context=context;
+            this.userManager = userManager;
             this.random = new Random();
         }
 
@@ -33,30 +33,49 @@ namespace WebFarmacia.Web.Data
             // verificando que la base de datos este creada
             await this.context.Database.EnsureCreatedAsync();
 
-            // registro de la base de datos
-            // await CheckPetypesAsync();
-            // await CheckOwnerAsync();
-            //await CheckMedicinaAsync();
+            //Creando el usuario
 
+            var user = await this.userManager.FindByEmailAsync("miguelrojas8143@gmail.com");
+            if (user == null)
+            {
+                user = new User
+                {
+                    FirstName = "Miguel",
+                    LasName = "Rojas",
+                    Email = "miguelrojas8143@gmail.com",
+                    UserName = "miguelrojas8143@gmail.com",
+                    PhoneNumber="04266828897"
+
+                };
+
+
+                var resul = await this.userManager.CreateAsync(user, "123456");
+                if (resul !=IdentityResult.Success)
+                {
+                    throw new InvalidOperationException("Could not create the user in seeder");
+                }
+
+            }
+        
             if (!this.context.Medicinas.Any())
             {
-                this.AddMedicina("Omeprazol");
-                this.AddMedicina("Atamel");
-                this.AddMedicina("Teragrip");
+                this.AddMedicina("Omeprazol", user);
+                this.AddMedicina("Atamel", user);
+                this.AddMedicina("Teragrip", user);
                 //grabando los cambios
                 await this.context.SaveChangesAsync();
             }
         }
 
-        private void AddMedicina(string name)
+        private void AddMedicina(string name,User user)
         {
             this.context.Medicinas.Add(new Medicina
             {
                 Name = name,
                 Price = this.random.Next(1000),
                 IsAvailabe = true,
-                stock = this.random.Next(100)
-                
+                stock = this.random.Next(100),
+                User = user
             });
         }
 
--- WebFarmacia.Web/Startup.cs
+++ WebFarmacia.Web/Startup.cs
@@ -1,19 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿namespace WebFarmacia.Web
+{using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using WebFarmacia.Web.Data;
+    using WebFarmacia.Web.Data;
+    using WebFarmacia.Web.Data.Entities;
 
-namespace WebFarmacia.Web
-{
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -26,10 +22,25 @@ namespace WebFarmacia.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddIdentity<User, IdentityRole>(cfg =>
+              {
+                  cfg.User.RequireUniqueEmail = true;
+                  cfg.Password.RequireDigit = false;
+                  cfg.Password.RequiredUniqueChars = 0;
+                  cfg.Password.RequireLowercase = false;
+                  cfg.Password.RequireNonAlphanumeric = false;
+                  cfg.Password.RequireUppercase = false;
+                  cfg.Password.RequiredLength = 6;
+
+              })
+                .AddEntityFrameworkStores<DataContext>();
+
+
             services.AddDbContext<DataContext>( cfg=>
 
                 {
-                    cfg.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"));
+                   cfg.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"));
 
             });
 
@@ -64,6 +75,7 @@ namespace WebFarmacia.Web
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseCookiePolicy();
 
             app.UseMvc(routes =>
